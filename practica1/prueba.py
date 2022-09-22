ff77a11740c41cc49310aa5d9018ceb6c31f8df8
Jesus Palomino <73486611+Jesus-Palomino@users.noreply.github.com>
2022-09-22 19:00:46 +0200

    Pseudocodigo para cifrado afin

--- practica1/prueba.py
+++ practica1/prueba.py
@@ -6,3 +6,47 @@ print(10 - mpz(1))
 print(gmpy2.is_prime(17))
 
 print("check")
+
+
+
+
+# ci = (a*mi + b) mod n
+
+# n = numero de letras
+
+# c = testo cifrado 
+
+# m = texto claro
+
+# b = constante de desplazamiento
+
+# a = coeficiente multiplicato
+
+
+
+# 1
+# Selector cifrado/descifrado
+# cinco valores de entrada
+
+# 2
+# Verificar que A y B MCD = 1 usando Euclides Extendido
+# Si no lo cumple = mensaje de error
+
+# 3
+# Asignar un estandar al parametro   i (fichero entrada)
+#                                    o (fichero salida)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
